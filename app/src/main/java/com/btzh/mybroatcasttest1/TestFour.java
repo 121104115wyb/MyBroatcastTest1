2b091b13f7e9efaef7e951107616ecbdb0793e09
yy <yaoyu26@foxmail.com>
2017-03-27 10:45:12 +0800

    新增了一个测试活动

---
+++ app/src/main/java/com/btzh/mybroatcasttest1/TestFour.java
@@ -0,0 +1,16 @@
+package com.btzh.mybroatcasttest1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Lenovo on 2017/3/27.
+ */
+
+public class TestFour extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constraintlayout_one);
+    }
+}
